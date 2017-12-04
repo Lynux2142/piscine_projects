cd2cffa4df5fddfde705eddcd6d83bb19c12e6b5
Lucas GUILLEROT <lguiller@e1r5p9.42.fr>
2017-12-04 19:13:55 +0100

    update fillit

--- fillit/srcs/fillit.h
+++ fillit/srcs/fillit.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fillit.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lguiller <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: manki <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/20 17:34:46 by lguiller          #+#    #+#             */
-/*   Updated: 2017/12/01 12:18:15 by lguiller         ###   ########.fr       */
+/*   Created: 2017/12/01 14:03:00 by manki             #+#    #+#             */
+/*   Updated: 2017/12/04 18:18:51 by manki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,35 @@
 
 # include "../libft/libft.h"
 
+typedef int		t_bool;
+
 typedef struct	s_coord
 {
 	int			x;
 	int			y;
 }				t_coord;
 
-typedef int		t_bool;
+typedef struct	s_tetris
+{
+	int					*id;
+	struct s_tetris		*next;
+}				t_tetris;
 
-char			*ft_stock_and_check_file(char *file);
-
-t_list			*ft_split_rec_tetri(char *tab);
-
-int				ft_nb_tetri(char *tab);
-int				ft_nb_row(char *tab);
 int				ft_sqrt(int nb);
 
-t_list			*ft_solve_grid(t_list *tetris, char **grid, int g_len);
+char			*ft_checkfile(char *file);
+int				ft_nb_row(char *tab);
 
-void			ft_list_push_back(t_list **list, int *content, size_t c_size);
-int				ft_listlen(t_list *list);
-t_list			*ft_create_elem(int *content, size_t c_size);
-void			ft_print_list_content(t_list *list, size_t c_size);
+t_tetris		*ft_create_elem(int *content);
+void			ft_push_back(t_tetris **tetris, int *content);
+void			ft_print_tetris(t_tetris *tetris);
+t_tetris		*ft_free_tetris(t_tetris *tetris);
+int				ft_nb_tetris(t_tetris *tetris);
 
+t_tetris		*ft_record_tetris(char *tab);
+
+char			**ft_resolve_grid(t_tetris *tetris);
 void			ft_print_result(char **grid, int len);
-char			**ft_create_square(int len);
+t_bool			ft_recursive(t_tetris *tetris, char **grid, int l, t_coord *g);
 
 #endif
