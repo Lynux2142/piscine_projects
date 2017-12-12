5f56de99ba2b9ecc77db7b274f69cef2da9b5bbe
Lucas GUILLEROT <lguiller@e1r6p9.42.fr>
2017-12-12 12:42:41 +0100

    ajout d'une nouvelle fonction itoa_base

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: lguiller <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 17:16:27 by lguiller          #+#    #+#             */
-/*   Updated: 2017/12/07 10:23:08 by lguiller         ###   ########.fr       */
+/*   Updated: 2017/12/12 12:33:38 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,5 +83,7 @@ void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+
+char				*ft_itoa_base(int value, int base);
 
 #endif
