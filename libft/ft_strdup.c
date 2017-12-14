bedf802858ece7e5d04a64dd33778fab167c74ba
Lucas GUILLEROT <lguiller@e1r6p11.42.fr>
2017-12-14 11:36:24 +0100

    amelioration de ft_strdup.c

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: lguiller <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 13:54:13 by lguiller          #+#    #+#             */
-/*   Updated: 2017/11/10 14:46:45 by lguiller         ###   ########.fr       */
+/*   Updated: 2017/12/14 11:35:35 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,11 @@ char	*ft_strdup(const char *s1)
 	i = 0;
 	while (s1[i] != 0)
 		++i;
-	if (!(dst = malloc(sizeof(char) * (i + 1))))
+	if (!(dst = (char *)ft_memalloc(sizeof(char) * (i + 1))))
 		return (0);
-	i = 0;
-	while (s1[i] != '\0')
-	{
+	i = -1;
+	while (s1[++i] != '\0')
 		dst[i] = s1[i];
-		i++;
-	}
 	dst[i] = '\0';
 	return (dst);
 }
