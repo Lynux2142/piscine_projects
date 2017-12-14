8554022906f7750dd0634ee68b7b582c9142bec5
Lucas GUILLEROT <lguiller@e1r6p11.42.fr>
2017-12-14 13:29:31 +0100

    maj

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lguiller <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 11:09:46 by lguiller          #+#    #+#             */
-/*   Updated: 2017/12/13 13:04:33 by lguiller         ###   ########.fr       */
+/*   Updated: 2017/12/14 12:57:57 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,28 @@
 
 int		get_next_line(const int fd, char **line)
 {
-	char		buff[BUFF_SIZE];
+	char		*buff;
 	static int	curs = 0;
 	int			i;
 	int			save;
 
 	ft_memdel((void *)&(*line));
-	read(fd, buff, BUFF_SIZE);
+	if (!(buff = (char *)ft_memalloc(sizeof(char) * BUFF_SIZE)))
+		return (0);
+	if (!read(fd, buff, BUFF_SIZE))
+		return (0);
+	ft_putendl(ft_itoa(curs));
 	if (curs != 0)
 		++curs;
 	save = curs;
 	while (buff[curs] && buff[curs] != '\n')
 		++curs;
-	if (!(*line = (char *)ft_memalloc(sizeof(char) * ((curs - save) + 1))))
-		return (-1);
+	if (!(*line = ft_memalloc(sizeof(*line) * ((curs - save) + 1))))
+		return (0);
 	i = -1;
 	while ((++i + save) < curs)
 		*(*line + i) = buff[save + i];
 	*(*line + i) = '\0';
+	ft_memdel((void *)&buff);
 	return (0);
 }
--- Get_next_line/get_next_line.h
+++ Get_next_line/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: lguiller <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 11:10:43 by lguiller          #+#    #+#             */
-/*   Updated: 2017/12/13 11:15:44 by lguiller         ###   ########.fr       */
+/*   Updated: 2017/12/14 12:43:39 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 610
 
 int		get_next_line(const int fd, char **line);
 
