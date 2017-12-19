1a67461ac389cf51501bd59cc745539cf1ffe3d9
Lucas GUILLEROT <lguiller@e1r6p11.42.fr>
2017-12-19 17:02:54 +0100

    ++

--- Get_next_line/get_next_line.h
+++ Get_next_line/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: lguiller <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 11:10:43 by lguiller          #+#    #+#             */
-/*   Updated: 2017/12/19 15:39:21 by lguiller         ###   ########.fr       */
+/*   Updated: 2017/12/19 16:05:49 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 3
+# define BUFF_SIZE 2
 
 int		get_next_line(const int fd, char **line);
 
