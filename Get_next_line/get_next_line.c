000c9974a04522831252126eee97ecf4c1a3d9f0
Lucas GUILLEROT <lguiller@e1r6p11.42.fr>
2017-12-19 15:45:31 +0100

    ajout de la fonction memrealloc

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lguiller <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 11:09:46 by lguiller          #+#    #+#             */
-/*   Updated: 2017/12/18 16:10:46 by lguiller         ###   ########.fr       */
+/*   Updated: 2017/12/19 15:38:38 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,56 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int			get_next_line(const int fd, char **line)
+static char		*ft_memrealloc(void *s1, void *s2, int len)
+{
+	int		len_s1;
+	int		i;
+
+	if (s1)
+	{
+		len_s1 = ft_strlen((char *)s1);
+		if (!(s2 = (void *)ft_memalloc(sizeof(void) * (len_s1 + len))))
+			return (NULL);
+		i = -1;
+		while (*(char *)(s1 + (++i)))
+			*(char *)(s2 + i) = *(char *)(s1 + i);
+	}
+	else
+	{
+		if (!(s2 = (void *)ft_memalloc(sizeof(void) * len)))
+			return (NULL);
+	}
+	return (s2);
+}
+
+int				get_next_line(const int fd, char **line)
 {
 	static char	buff[BUFF_SIZE];
 	static int	curs = 0;
 	int			i;
 	int			save;
+	int			ok;
 
-	if (curs != 0)
-		++curs;
-	else
-		read(fd, buff, BUFF_SIZE);
-	save = curs;
-	while (buff[curs] && buff[curs] != '\n')
-		++curs;
-	if (!(*line = (char *)ft_memalloc(sizeof(char) * ((curs - save) + 1))))
-		return (0);
-	i = -1;
-	while ((++i + save) < curs)
-		*(*line + i) = buff[save + i];
-	*(*line + i) = '\0';
-	ft_bzero((void *)buff, (curs - save));
-	curs = (buff[curs] == '\0') ? 0 : curs;
+	ok = 0;
+	while (!ok)
+	{
+		if (curs != 0)
+			++curs;
+		else
+			read(fd, buff, BUFF_SIZE);
+		save = curs;
+		while (buff[curs] && buff[curs] != '\n')
+			++curs;
+		if (buff[curs] == '\n')
+			ok = 1;
+		if (!(*line = (char *)ft_memrealloc(*line, *line, sizeof(char) * ((curs - save) + 1))))
+			return (0);
+		i = -1;
+		while ((++i + save) < curs)
+			*(*line + i) = buff[save + i];
+		*(*line + i) = '\0';
+		ft_bzero((void *)buff, (curs - save));
+		curs = (buff[curs] == '\0') ? 0 : curs;
+	}
 	return (1);
 }
