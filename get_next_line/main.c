c1e4272ea25268ee3f805af62ab8b5e83d1b0e76
Lucas GUILLEROT <lguiller@e1r6p9.42.fr>
2017-12-23 13:30:51 +0100

    update get_next_line

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: lguiller <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/22 15:16:24 by lguiller          #+#    #+#             */
-/*   Updated: 2017/12/23 12:06:43 by lguiller         ###   ########.fr       */
+/*   Updated: 2017/12/23 13:25:31 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,15 @@ int		main(int ac, char **av)
 {
 	int		fd;
 	char	*line;
-	int		result;
 
 	(void)ac;
 	if ((fd = open(av[1], O_RDONLY)) == -1)
 		return (-1);
-	result = 1;
-	while (result > 0)
+	while (get_next_line((const int)fd, &line))
 	{
-		result = get_next_line((const int)fd, &line);
 		ft_putendl(line);
 		ft_memdel((void *)&line);
 	}
 	close(fd);
-	return (0);
+	return (get_next_line((const int)fd, &line));
 }
