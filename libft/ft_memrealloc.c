0791b27cf0a6920cc3447b5f2a2b3124582af8f6
Lucas GUILLEROT <lguiller@e1r5p11.42.fr>
2017-12-29 11:53:19 +0100

    modification memrealloc

--- libft/ft_memrealloc.c
+++ libft/ft_memrealloc.c
@@ -6,7 +6,7 @@
 /*   By: lguiller <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/22 13:29:00 by lguiller          #+#    #+#             */
-/*   Updated: 2017/12/29 11:19:09 by lguiller         ###   ########.fr       */
+/*   Updated: 2017/12/29 11:51:46 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,20 @@ void	*ft_memrealloc(void *str, size_t size)
 	void	*tmp;
 	int		i;
 
-	tmp = str;
-	if (!(str = ft_memalloc(size + 1)))
-		return (NULL);
-	if (tmp != NULL)
+	if (size == 0 && str)
+		ft_memdel(&str);
+	else
 	{
-		i = -1;
-		while (*(char *)&tmp[++i])
-			*(char *)&str[i] = *(char *)&tmp[i];
+		tmp = str;
+		if (!(str = ft_memalloc(size)))
+			return (NULL);
+		if (tmp)
+		{
+			i = -1;
+			while (*(char *)&tmp[++i])
+				*(char *)&str[i] = *(char *)&tmp[i];
+			ft_memdel(&tmp);
+		}
 	}
-	ft_memdel(&tmp);
 	return (str);
 }
