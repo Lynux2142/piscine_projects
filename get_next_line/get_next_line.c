42b61ec5cf6ff5c755cf2134a8021a9b5ce8ff95
Lucas GUILLEROT <lguiller@e1r5p13.42.fr>
2018-01-10 16:42:56 +0100

    ajout du bonus 1 variable static sur GNL

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lguiller <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/22 15:24:31 by lguiller          #+#    #+#             */
-/*   Updated: 2018/01/10 15:51:06 by lguiller         ###   ########.fr       */
+/*   Updated: 2018/01/10 16:30:15 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,42 @@
 #include "get_next_line.h"
 #include <unistd.h>
 
-static char	ft_getchar(const int fd, int *rd_len)
+static char	ft_getchar(const int fd, int *rd_len, t_struct *var)
 {
-	static char	buff[BUFF_SIZE];
-	static char	*str;
-	static int	len = 0;
 	char		c;
 
-	if (len == 0)
+	if (var->len == 0)
 	{
-		if ((*rd_len = read(fd, buff, BUFF_SIZE)) <= 0)
+		if ((*rd_len = read(fd, var->buff, BUFF_SIZE)) <= 0)
 			return ((*rd_len == 0) ? 0 : -1);
-		len = *rd_len;
-		str = (char *)&buff;
+		var->len = *rd_len;
+		var->str = (char *)&var->buff;
 	}
-	c = str[0];
-	++str;
-	--len;
+	c = var->str[0];
+	++var->str;
+	--var->len;
 	return (c);
 }
 
 int			get_next_line(const int fd, char **line)
 {
-	char		c;
-	int			len;
-	static int	rd_len = 0;
+	static t_struct	var;
+	char			c;
+	int				len;
 
 	if (line == NULL || fd < 0)
 		return (-1);
 	len = 0;
 	if (!(*line = ft_strnew(BUFF_SIZE)))
 		return (0);
-	if ((c = ft_getchar(fd, &rd_len)) == -1)
+	if ((c = ft_getchar(fd, &var.rd_len, &var)) == -1)
 		return (-1);
-	while (c != '\n' && rd_len > 0)
+	while (c != '\n' && var.rd_len > 0)
 	{
 		if (c == -1)
 			return (-1);
 		line[0][len] = c;
-		c = ft_getchar(fd, &rd_len);
+		c = ft_getchar(fd, &var.rd_len, &var);
 		++len;
 		if ((len % BUFF_SIZE) == 0)
 			*line = ft_realloc(*line, len + BUFF_SIZE + 1);
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,15 +6,23 @@
 /*   By: lguiller <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/21 11:16:50 by lguiller          #+#    #+#             */
-/*   Updated: 2018/01/10 14:27:17 by lguiller         ###   ########.fr       */
+/*   Updated: 2018/01/10 16:39:32 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 42
+# define BUFF_SIZE 12
 
-int		get_next_line(const int fd, char **line);
+typedef struct	s_struct
+{
+	char		buff[BUFF_SIZE];
+	char		*str;
+	int			len;
+	int			rd_len;
+}				t_struct;
+
+int				get_next_line(const int fd, char **line);
 
 #endif
