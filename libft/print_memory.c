5652a1655e38b9cbd58449a774cd55141e846201
Lucas GUILLEROT <lguiller@e1r6p11.42.fr>
2018-01-21 16:27:42 +0100

    modif print_memory

--- libft/print_memory.c
+++ libft/print_memory.c
@@ -6,25 +6,21 @@
 /*   By: lguiller <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/07 10:10:33 by lguiller          #+#    #+#             */
-/*   Updated: 2018/01/09 12:16:00 by lguiller         ###   ########.fr       */
+/*   Updated: 2018/01/21 16:27:11 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "libft.h"
 
-static void	ft_puthex(unsigned char c)
+static int	ft_puthex(unsigned char c)
 {
 	char *tab;
 
 	tab = "0123456789abcdef";
 	ft_putchar(tab[c / 16]);
 	ft_putchar(tab[c % 16]);
-}
-
-static void	ft_print(unsigned char c)
-{
-	ft_putchar(ft_isprint(c) ? c : '.');
+	return (1);
 }
 
 static void	print_line(unsigned char *str, size_t start, size_t max)
@@ -32,23 +28,19 @@ static void	print_line(unsigned char *str, size_t start, size_t max)
 	size_t i;
 
 	i = start - 1;
-	while (++i < (start + 16) && i < max)
-	{
-		ft_puthex(str[i]);
+	while (++i < (start + 16) && i < max && ft_puthex(str[i]))
 		if (i % 2)
 			ft_putchar(' ');
-	}
 	while (i < (start + 16))
 	{
-		ft_putchar(' ');
-		ft_putchar(' ');
+		ft_putstr("  ");
 		if (i % 2)
 			ft_putchar(' ');
 		i++;
 	}
 	i = start - 1;
 	while (++i < (start + 16) && i < max)
-		ft_print(str[i]);
+		ft_putchar(ft_isprint(str[i]) ? str[i] : '.');
 	ft_putchar('\n');
 }
 
