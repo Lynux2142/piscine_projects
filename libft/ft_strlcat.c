347aa3e8976c44651b35f3793eeb053fc203f687
Lucas GUILLEROT <lguiller@e1r5p13.42.fr>
2018-01-24 12:20:32 +0100

    update libft

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: lguiller <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 14:58:30 by lguiller          #+#    #+#             */
-/*   Updated: 2018/01/06 10:39:09 by lguiller         ###   ########.fr       */
+/*   Updated: 2018/01/24 11:56:51 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,10 @@ size_t		ft_strlcat(char *dst, const char *src, size_t size)
 	while (dst[j] && j < size)
 		++j;
 	if (j == size)
-		return (ft_strlen(src) + size);
-	i = 0;
-	while (src[i] && ((j + i) < (size - 1)))
-	{
+		return (ft_strlen((char *)src) + size);
+	i = -1;
+	while (src[++i] && ((j + i) < (size - 1)))
 		dst[j + i] = src[i];
-		++i;
-	}
 	dst[j + i] = '\0';
 	return (ft_strlen((char *)src) + j);
 }
