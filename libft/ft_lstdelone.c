c7a5f445007a04e604f1fa4862b1cddfc0a3c367
Lucas GUILLEROT <lguiller@e1r5p13.42.fr>
2018-03-12 12:17:39 +0100

    mise a jour libft

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: lguiller <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 12:35:27 by lguiller          #+#    #+#             */
-/*   Updated: 2017/12/07 11:07:52 by lguiller         ###   ########.fr       */
+/*   Updated: 2018/03/12 12:12:47 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@ void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
 	if (alst != NULL && *alst != NULL)
 	{
-		del((*alst)->content, 0);
-		free(*alst);
-		*alst = NULL;
+		del((*alst)->content, (*alst)->content_size);
+		ft_memdel((void **)alst);
 	}
 }
